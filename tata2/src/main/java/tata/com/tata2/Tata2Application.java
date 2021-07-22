75e04fc30f7da215d307fadbe0bd823869633d64
suresh a <asureshdevops10@gmail.com>
2021-07-22 11:47:28 +0530

    tata2

---
+++ tata2/src/main/java/tata/com/tata2/Tata2Application.java
@@ -0,0 +1,13 @@
+package tata.com.tata2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tata2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tata2Application.class, args);
+	}
+
+}
---
+++ tata2/src/test/java/tata/com/tata2/Tata2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tata.com.tata2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tata2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
